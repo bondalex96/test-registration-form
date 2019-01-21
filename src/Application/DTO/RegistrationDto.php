232268952f47cf31ce5dcaf812e7a6426c65b393
Bondarenko Alexandr <boondalex96@gmail.com>
2019-01-21 19:19:19 +0100

    Implemented Form and Dto validation.

--- src/Application/DTO/RegistrationDto.php
+++ src/Application/DTO/RegistrationDto.php
@@ -2,13 +2,35 @@
 
 namespace App\Application\DTO;
 
+use Symfony\Component\Validator\Constraints as Assert;
 
 class RegistrationDto
 {
+    /**
+     * @Assert\NotBlank
+     */
     public $nick;
+
+    /**
+     * @Assert\NotBlank
+     */
     public $firstName;
+
+    /**
+     * @Assert\NotBlank
+     */
     public $lastName;
+
+    /**
+     * @Assert\NotBlank
+     * @Assert\Email
+     */
     public $email;
+
+    /**
+     * @Assert\NotBlank
+     * @Assert\Length(min="5")
+     */
     public $password;
 
     public static function create(
